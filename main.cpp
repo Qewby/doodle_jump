458aec4274bb8f6200e8acea7604e62aab436e1e
Bohdan Tsehelnyk <mail.qewby@gmail.com>
2021-05-11 18:24:19 +0300

    Create "Hello world" window.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
 
+#include "SDL2/SDL.h"
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 int main()
 {
-    std::cout<<"Hello, World!"<<std::endl;
+    bool quit = false;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cout << "Can't init SDL: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    window = SDL_CreateWindow("Hello world", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        std::cout << "Can't create window: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, NULL);
+    if (renderer == NULL) {
+        std::cout << "Can't create renderer: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Event event;
+    while(!quit)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            SDL_PumpEvents();
+            if(event.type == SDL_QUIT)
+                quit=true;
+        }
+        SDL_RenderClear( renderer );
+        SDL_RenderPresent( renderer );
+    }
+
+    SDL_DestroyRenderer( renderer );
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
