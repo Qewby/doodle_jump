7bb6f36b4e83dcfe9b8ff21181d3eb4899af6a35
Bohdan Tsehelnyk <mail.qewby@gmail.com>
2021-06-02 19:58:05 +0300

    Add mouse motion and left click event

--- EventListener.cpp
+++ EventListener.cpp
@@ -1,6 +1,8 @@
 #include "EventListener.h"
 
-std::map<std::string, bool> gKeyStatesMap;
+std::map<std::string, bool> gKeyStatesMap{};
+std::pair<int, int> gLeftMouseClickPosition{};
+std::pair<int, int> gMousePosition{};
 
 EventListener::EventListener(bool& quit) : mQuit(quit) {
     mpKeyboardStateArray = SDL_GetKeyboardState(NULL);
@@ -22,6 +24,14 @@ void EventListener::Listen() {
                 gKeyStatesMap["left"] = mpKeyboardStateArray[SDL_SCANCODE_LEFT] || mpKeyboardStateArray[SDL_SCANCODE_A];
                 gKeyStatesMap["right"] = mpKeyboardStateArray[SDL_SCANCODE_RIGHT] || mpKeyboardStateArray[SDL_SCANCODE_D];
                 break;
+            case SDL_MOUSEBUTTONUP:
+                if (mEvent.button.button == SDL_BUTTON_LEFT) {
+                    gLeftMouseClickPosition = {mEvent.button.x, mEvent.button.y};
+                    SDL_Log("%d, %d", gLeftMouseClickPosition.first,
+                            gLeftMouseClickPosition.second);
+                }
+            case SDL_MOUSEMOTION:
+                gMousePosition = {mEvent.motion.x, mEvent.motion.y};
         }
     }
 }
