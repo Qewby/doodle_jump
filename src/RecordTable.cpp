c7a070fb82ea30aaf1521d7d2dae02a5b9548dd3
Bohdan Tsehelnyk <mail.qewby@gmail.com>
2021-06-10 15:17:56 +0300

    Make RecordTable drawable

--- src/RecordTable.cpp
+++ src/RecordTable.cpp
@@ -1,11 +1,18 @@
 #include "RecordTable.h"
 
-RecordTable::RecordTable() : mTable() {
+const int RecordTable::scIndent = 5;
+const int RecordTable::scMaxCharCount = 10;
 
+RecordTable::RecordTable() : mTable() {
+    mpFont = TTF_OpenFont("assets/fonts/font.ttf", 80);
+    mNameHitBox.w = mScoreHitBox.w = WINDOW_WIDTH / 4;
+    mNameHitBox.h = mScoreHitBox.h = WINDOW_HEIGHT / 25;
+    mNameHitBox.x = (WINDOW_WIDTH - mNameHitBox.w) / 3;
+    mScoreHitBox.x = mNameHitBox.x + mNameHitBox.w + scIndent;
 }
 
 RecordTable::~RecordTable() {
-
+    TTF_CloseFont(mpFont);
 }
 
 void RecordTable::ReadTable() {
@@ -57,4 +64,35 @@ void RecordTable::UpdateTable(std::string name) {
 
 bool RecordTable::IsRecord() {
     return SCORE > mTable.back().score;
+}
+
+void RecordTable::Draw(Renderer &renderer) {
+    mNameHitBox.y = mScoreHitBox.y = (WINDOW_HEIGHT - ((mNameHitBox.h + scIndent) * mTable.size() - scIndent)) / 2;
+    for (auto record : mTable) {
+        std::string scoreText = std::to_string(record.score);
+        int emptySize = scMaxCharCount - scoreText.length();
+        for (int i = 0; i < emptySize; i++) {
+            scoreText += " ";
+        }
+
+        std::string nicknameText = record.nickname;
+        emptySize = scMaxCharCount - nicknameText.length();
+        for (int i = 0; i < emptySize; i++) {
+            nicknameText += " ";
+        }
+
+        mpSurface = TTF_RenderText_Solid(mpFont, nicknameText.c_str(), {0, 0, 0});
+        mpTexture = SDL_CreateTextureFromSurface(renderer.GetRawRenderer(), mpSurface);
+        SDL_RenderCopy(renderer.GetRawRenderer(), mpTexture, NULL, &mNameHitBox);
+        SDL_FreeSurface(mpSurface);
+        SDL_DestroyTexture(mpTexture);
+
+        mpSurface = TTF_RenderText_Solid(mpFont, scoreText.c_str(), {0, 0, 0});
+        mpTexture = SDL_CreateTextureFromSurface(renderer.GetRawRenderer(), mpSurface);
+        SDL_RenderCopy(renderer.GetRawRenderer(), mpTexture, NULL, &mScoreHitBox);
+        SDL_FreeSurface(mpSurface);
+        SDL_DestroyTexture(mpTexture);
+
+        mNameHitBox.y = mScoreHitBox.y = mNameHitBox.y + mNameHitBox.h + scIndent;
+    }
 }
--- src/RecordTable.h
+++ src/RecordTable.h
@@ -4,10 +4,14 @@
 #include <fstream>
 #include <array>
 
+#include "SDL_ttf.h"
+
+#include "IDrawable.h"
+
 extern unsigned long long  SCORE;
 extern unsigned long long  RECORD;
 
-class RecordTable {
+class RecordTable : public IDrawable {
 public:
     RecordTable();
     ~RecordTable();
@@ -15,12 +19,23 @@ public:
     void ReadTable();
     void UpdateTable(std::string name);
     bool IsRecord();
+
+    void Draw(Renderer &renderer) override;
 private:
     struct Record {
         std::string nickname;
         unsigned long long score;
     };
     std::array<Record, 5> mTable;
+
+    static const int scIndent;
+    TTF_Font *mpFont;
+    SDL_Rect mNameHitBox;
+    SDL_Rect mScoreHitBox;
+    SDL_Surface *mpSurface;
+    SDL_Texture *mpTexture;
+
+    static const int scMaxCharCount;
 };
 
 
