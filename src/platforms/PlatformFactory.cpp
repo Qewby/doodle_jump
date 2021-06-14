a88af7eb6aa93c08b4fd26cf1cc966f05e04d17e
Bohdan Tsehelnyk <mail.qewby@gmail.com>
2021-06-14 18:48:20 +0300

    Refactor: change platform creating algo

--- src/platforms/PlatformFactory.cpp
+++ src/platforms/PlatformFactory.cpp
@@ -1,8 +1,10 @@
 #include "PlatformFactory.h"
 
+extern unsigned long long SCORE;
+
 PlatformFactory::PlatformFactory(Renderer& renderer) : mrRenderer(renderer) {
     mCountEmptyInRaw = 1;
-    MovingPlatform::SetSpeed(4);
+    MovingPlatform::SetSpeed(6);
 }
 
 PlatformFactory::~PlatformFactory() {
@@ -12,9 +14,23 @@ PlatformFactory::~PlatformFactory() {
 Platform* PlatformFactory::CreatePlatform(int x, int y) {
     std::random_device randomizer{};
     Platform *newPlatform = nullptr;
+
+    int a;
     while (!newPlatform) {
+        if (SCORE > 8000) {
+            a = 4;
+        }
+        else if (SCORE > 5000) {
+            a = 3;
+        }
+        else if (SCORE > 2000) {
+            a = 2;
+        }
+        else {
+            a = 1;
+        }
         if (mCountEmptyInRaw % 4 == 0) {
-            switch (randomizer() % 4) {
+            switch (randomizer() % a) {
                 case 0:
                     newPlatform = new SimplePlatform(x, y, mrRenderer);
                     break;
